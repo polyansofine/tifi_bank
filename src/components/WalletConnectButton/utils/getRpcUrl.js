80ed143ef6328f6530af8c5054dce126305acddc
Yan <93109473+polyansofine@users.noreply.github.com>
2022-06-27 08:24:37 +0300

    nodes

--- src/components/WalletConnectButton/utils/getRpcUrl.js
+++ src/components/WalletConnectButton/utils/getRpcUrl.js
@@ -2,9 +2,9 @@ import sample from "lodash/sample";
 
 // Array of available nodes to connect to
 export const nodes = [
-  "https://data-seed-prebsc-1-s1.binance.org:8545",
-  "https://data-seed-prebsc-1-s1.binance.org:8545",
-  "https://data-seed-prebsc-1-s1.binance.org:8545",
+  "https://data-seed-prebsc-1-s3.binance.org:8545",
+  "https://data-seed-prebsc-1-s3.binance.org:8545",
+  "https://data-seed-prebsc-1-s3.binance.org:8545",
 ];
 // export const nodes = process.env.REACT_APP_NODE_1
 const getNodeUrl = () => {
